e10bfe1ed68b58b13f55a080dba2b62eb63216ad
joel thomas mathew <joelmathew857@gmail.com>
2023-07-19 15:28:23 +0530

    add button with color

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,46 @@
 import React, { useState } from "react";
 
-function ColorChange() {
-  const [color, setColor] = useState("blue"); // State for color
+const ColorChange = () => {
+  const [color, setColor] = useState("green"); // State for color
 
   const changeColor = () => {
-    setColor("red"); // Change color to red
+    if (color === "green") {
+      setColor("yellow");
+    } else if (color === "yellow") {
+      setColor("blue");
+    } else if (color === "blue") {
+      setColor("red");
+    } else {
+      setColor("green");
+    }
+  };
+
+  const getColorStyles = () => {
+    switch (color) {
+      case "green":
+        return "bg-green-500 text-white";
+      case "yellow":
+        return "bg-yellow-500 text-black";
+      case "blue":
+        return "bg-blue-500 text-white";
+      case "red":
+        return "bg-red-500 text-white";
+      default:
+        return "";
+    }
   };
 
   return (
     <div>
-      <h1 style={{ color: color }}>Color Change Example</h1>
-      <button onClick={changeColor}>Change Color</button>
+      <h1 className={`text-3xl font-bold ${getColorStyles()}`}>{color}</h1>
+      <button
+        className="px-4 py-2 mt-4 bg-gray-500 text-white rounded"
+        onClick={changeColor}
+      >
+        Change Color
+      </button>
     </div>
   );
-}
+};
 
 export default ColorChange;
-
-
