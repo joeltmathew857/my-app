7b5790a216b75e4eb2f6cb9775d670fd0f1d1065
joel thomas mathew <joelmathew857@gmail.com>
2023-07-19 19:03:44 +0530

    add usestate-colorchange

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const ColorChange = () => {
   const [color, setColor] = useState("green"); // State for color
@@ -15,30 +15,65 @@ const ColorChange = () => {
     }
   };
 
-  const getColorStyles = () => {
+  const resetColor = () => {
+    setColor("green");
+  };
+
+  const generateRandomColor = () => {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    setColor(randomColor);
+  };
+
+  const getColorName = () => {
     switch (color) {
       case "green":
-        return "bg-green-500 text-white";
+        return "Green";
       case "yellow":
-        return "bg-yellow-500 text-black";
+        return "Yellow";
       case "blue":
-        return "bg-blue-500 text-white";
+        return "Blue";
       case "red":
-        return "bg-red-500 text-white";
+        return "Red";
       default:
         return "";
     }
   };
 
+  useEffect(() => {
+    // Perform any side effects or subscriptions here
+    // This effect runs only once after the initial render
+
+    // Cleanup function (optional)
+    return () => {
+      // Perform cleanup here
+      // This function is called when the component is unmounted
+    };
+  }, []); // Empty dependency array means the effect runs only once
+
   return (
     <div>
-      <h1 className={`text-3xl font-bold ${getColorStyles()}`}>{color}</h1>
-      <button
-        className="px-4 py-2 mt-4 bg-gray-500 text-white rounded"
-        onClick={changeColor}
-      >
-        Change Color
-      </button>
+      <h1 className="text-4xl font-bold">{getColorName()}</h1>
+      <div className={`w-20 h-20 rounded bg-${color}`}></div>
+      <div className="flex mt-4">
+        <button
+          className="px-4 py-2 mr-2 bg-gray-500 text-white rounded"
+          onClick={changeColor}
+        >
+          Change Color
+        </button>
+        <button
+          className="px-4 py-2 mr-2 bg-gray-500 text-white rounded"
+          onClick={resetColor}
+        >
+          Reset Color
+        </button>
+        <button
+          className="px-4 py-2 bg-gray-500 text-white rounded"
+          onClick={generateRandomColor}
+        >
+          Random Color
+        </button>
+      </div>
     </div>
   );
 };
