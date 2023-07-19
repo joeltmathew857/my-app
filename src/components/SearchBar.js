34e153f3157a0fbeaff3e99b869fc19fea2ae866
joel thomas mathew <joelmathew857@gmail.com>
2023-07-19 10:38:57 +0530

    added time

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-const SearchBar = () => {
+const Clock = () => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const timerID = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timerID);
+    };
+  }, []);
+
   return (
     <div>
-      {/* <input
-        type="search"
-        id="default-search"
-        className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      />
-      <button
-        type="submit"
-        className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Search
-      </button> */}
+      <h1>Hello, world!</h1>
+      <h2>It is {date.toLocaleTimeString()}.</h2>
     </div>
   );
 };
 
-export default SearchBar;
+export default Clock;
