9d13aa2790244d50224b886f7da3997aef1d9886
joel thomas mathew <joelmathew857@gmail.com>
2023-07-20 01:34:08 +0530

    add-color-button

--- src/components/NewButton.js
+++ src/components/NewButton.js
@@ -3,12 +3,8 @@ import React, { useState } from "react";
 const MathButton = () => {
   const [count, setCount] = useState(0);
 
-  const incrementCount = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
-
-  const decrementCount = () => {
-    setCount((prevCount) => prevCount - 1);
+  const updateCount = (value) => {
+    setCount((prevCount) => prevCount + value);
   };
 
   const resetCount = () => {
@@ -17,24 +13,24 @@ const MathButton = () => {
 
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <div className="w-64 h-64 bg-gray-200 rounded-lg shadow-lg flex flex-col items-center justify-center">
+      <div className="w-80 h-80 bg-gray-200 rounded-lg shadow-lg flex flex-col items-center justify-center">
         <h2 className="text-2xl font-bold text-black">Count: {count}</h2>
         <div className="mt-8">
           <div className="flex mt-4">
             <button
-              className="px-4 py-2 mr-4 bg-gray-500 text-white rounded"
-              onClick={incrementCount}
+              className="px-2 py-1 mr-4 bg-green-500 text-white rounded"
+              onClick={() => updateCount(1)}
             >
               Increment
             </button>
             <button
-              className="px-4 py-2 mr-4 bg-gray-500 text-white rounded"
-              onClick={decrementCount}
+              className="px-2 py-1 mr-4 bg-red-500 text-white rounded"
+              onClick={() => updateCount(-1)}
             >
               Decrement
             </button>
             <button
-              className="px-4 py-2 bg-gray-500 text-white rounded"
+              className="px-2 py-1 bg-gray-500 text-white rounded"
               onClick={resetCount}
             >
               Reset
