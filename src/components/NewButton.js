7cf5e6bdd0390c673dbf010557abdcc4b7802c4c
joel thomas mathew <joelmathew857@gmail.com>
2023-07-19 21:44:49 +0530

    add-decrementCount-button

--- src/components/NewButton.js
+++ src/components/NewButton.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const ChangeColor = () => {
+const MathButton = () => {
   const [count, setCount] = useState(0);
 
   const incrementCount = () => {
@@ -17,31 +17,33 @@ const ChangeColor = () => {
 
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <h2 className="text-2xl font-bold text-black">Count: {count}</h2>
-      <div className="mt-8">
-        <div className="flex mt-4">
-          <button
-            className="px-4 py-2 mr-4 bg-gray-500 text-white rounded"
-            onClick={incrementCount}
-          >
-            Increment Count
-          </button>
-          <button
-            className="px-4 py-2 mr-4 bg-gray-500 text-white rounded"
-            onClick={decrementCount}
-          >
-            Decrement Count
-          </button>
-          <button
-            className="px-4 py-2 bg-gray-500 text-white rounded"
-            onClick={resetCount}
-          >
-            Reset Count
-          </button>
+      <div className="w-64 h-64 bg-gray-200 rounded-lg shadow-lg flex flex-col items-center justify-center">
+        <h2 className="text-2xl font-bold text-black">Count: {count}</h2>
+        <div className="mt-8">
+          <div className="flex mt-4">
+            <button
+              className="px-4 py-2 mr-4 bg-gray-500 text-white rounded"
+              onClick={incrementCount}
+            >
+              Increment
+            </button>
+            <button
+              className="px-4 py-2 mr-4 bg-gray-500 text-white rounded"
+              onClick={decrementCount}
+            >
+              Decrement
+            </button>
+            <button
+              className="px-4 py-2 bg-gray-500 text-white rounded"
+              onClick={resetCount}
+            >
+              Reset
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default ChangeColor;
+export default MathButton;
